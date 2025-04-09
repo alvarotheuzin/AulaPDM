b9db045ba1a4e3f133e41b71c67d68eebe1fe535
alvaro.s@iesb.edu.br <23214290020@ACADEMICO.IESB.BR>
2025-04-09 20:59:43 -0300

    08-navegação

--- 08-navegacao-abas/App.js
+++ 08-navegacao-abas/App.js
@@ -1,9 +1,11 @@
 import { PaperProvider } from 'react-native-paper';
 import {NavigationContainer} from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons'
 
 import HomeScreen from './screens/HomeScreen'
 import ProfileScreen from './screens/ProfileScreen';
+import SettingsScreen from './screens/SettingsScreen';
 
 const Tab = createBottomTabNavigator()
 
@@ -14,9 +16,23 @@ export default function App() {
       <NavigationContainer>
         <Tab.Navigator>
 
-          <Tab.Screen name='HomeScreen' component={HomeScreen}/> 
+          <Tab.Screen
+          name='HomeScreen'
+          component={HomeScreen}
+          options={{
+            title: 'Tela de Início',
+            headerTitleAlign: 'center',
+            headerTintColor: 'white',
+            headerStyle: {
+              backgroundColor: 'red'
+            },
+            tabBarIcon: ({color, size}) => <Ionicons name= 'home' color={color} size={size} />
+          }}
+          /> 
 
           <Tab.Screen name='ProfileScreen' component={ProfileScreen}/> 
+
+          <Tab.Screen name='SettingsScreen' component={SettingsScreen}/> 
        
         </Tab.Navigator>
       </NavigationContainer>
--- 08-navegacao-abas/screens/HomeScreen.jsx
+++ 08-navegacao-abas/screens/HomeScreen.jsx
@@ -1,14 +1,56 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { ScrollView, StyleSheet, View } from 'react-native'
+import { Text, Card, title, Paragraph, Title} from 'react-native-paper'
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>HomeScreen</Text>
+
+    <ScrollView>
+    <View style={styles.container}>
+
+      <Text variant='headlineSmall' style={{ fontWeight: 'bold'}}>Inicio</Text>
+
+      <Card>
+        <Card.Content>
+          <Title>Um Titulo</Title>
+          <Paragraph>
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+          </Paragraph>
+        </Card.Content>
+        <Card.Cover source={{ uri: 'https://picsum.photos/700'}}></Card.Cover>
+      </Card>
+
+      <Card>
+        <Card.Content>
+          <Title>Um Titulo</Title>
+          <Paragraph>
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+          </Paragraph>
+        </Card.Content>
+        <Card.Cover source={{ uri: 'https://picsum.photos/700'}}></Card.Cover>
+      </Card>
+
+      <Card>
+        <Card.Content>
+          <Title>Um Titulo</Title>
+          <Paragraph>
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+          </Paragraph>
+        </Card.Content>
+        <Card.Cover source={{ uri: 'https://picsum.photos/700'}}></Card.Cover>
+      </Card>
+    
     </View>
+  </ScrollView>
   )
 }
 
 
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'fff',
+    alignItems: 'center',
+    paddingTop: 10
+  }
+})
